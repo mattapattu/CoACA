c4ec891bae0f6f9e9ea1f10b4932a45f33bb0280
mattapattu <mattpattu@gmail.com>
2021-07-14 22:01:56 +0200

    latest

---
+++ Sources/lib/TurnsNew/src/TurnsNew.cpp
@@ -0,0 +1,52 @@
+#include "aca3.hpp"
+#include "sarsa.hpp"
+
+// [[Rcpp::export()]]
+Rcpp::List simulateTurnsModels(Rcpp::S4 ratdata, Rcpp::S4 modelData, Rcpp::S4 testModel, arma::vec turnStages, bool debug = false)
+{
+    std::string creditAssignment = Rcpp::as<std::string>(modelData.slot("creditAssignment"));
+    Rcpp::List ret;
+    if(creditAssignment == "aca3")
+    {
+      ret =   simulateAca3TurnsModels(ratdata, modelData, testModel, turnStages, debug);
+    }
+    else if(creditAssignment == "sarsa")
+    {
+
+    }
+    return(ret);
+}
+
+// [[Rcpp::export()]]
+std::vector<double> getTurnsLikelihood(Rcpp::S4 ratdata, Rcpp::S4 modelData, Rcpp::S4 testModel, int sim)
+{
+    std::string model = Rcpp::as<std::string>(modelData.slot("Model"));
+    std::string creditAssignment = Rcpp::as<std::string>(modelData.slot("creditAssignment"));
+    std::vector<double> ret;
+    if(creditAssignment == "aca3")
+    {
+      ret =   getAca3Likelihood(ratdata, modelData, testModel, sim);
+    }
+    else if(creditAssignment == "sarsa")
+    {
+        ret =   getSarsaLik(ratdata, modelData, testModel, sim);
+    }
+    return(ret);
+}
+
+// [[Rcpp::export()]]
+arma::mat getProbMatrix(Rcpp::S4 ratdata, Rcpp::S4 modelData, Rcpp::S4 testModel, int sim, bool debug = false)
+{
+    std::string model = Rcpp::as<std::string>(modelData.slot("Model"));
+    std::string creditAssignment = Rcpp::as<std::string>(modelData.slot("creditAssignment"));
+    arma::mat ret;
+    if(creditAssignment == "aca3")
+    {
+      ret =   getAca3ProbMatrix(ratdata, modelData, testModel, sim, debug);
+    }
+    else if(creditAssignment == "sarsa")
+    {
+        ret =   getSarsaProbMat(ratdata, modelData, testModel, sim);
+    }
+    return(ret);
+}

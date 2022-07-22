120298c8101b2ea271d6f5337107ff05a33779fa
mattapattu <34416958+mattapattu@users.noreply.github.com>
2022-07-22 15:56:11 +0200

    latest

--- Sources/src/ValidationFunc2.R
+++ Sources/src/ValidationFunc2.R
@@ -112,7 +112,7 @@ HoldoutTestNew=function(ratdata,testData,src.dir,setup.hpc,model.data.dir,seed,c
   # rat = ratdata@rat
   # save(resList,  file = paste0(res.model.data.dir,"/",rat, format(Sys.time(),'_%Y%m%d_%H%M%S_'),testDataName,"_resList.Rdata"))
 setwd(res.model.data.dir)
-print(resData)
+print(res.model.data.dir)
 rat=ratdata@rat
  resData=list.files(".", pattern = paste0(rat, ".*Mix_resList.Rdata"))
  print(resData)

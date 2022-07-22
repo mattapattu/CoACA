a64a28dc6b29f230e1aa625b46f295e60a111bd2
mattapattu <34416958+mattapattu@users.noreply.github.com>
2022-07-22 11:21:09 +0200

    latest

--- Sources/src/ValidationFunc2.R
+++ Sources/src/ValidationFunc2.R
@@ -21,102 +21,97 @@ HoldoutTestNew=function(ratdata,testData,src.dir,setup.hpc,model.data.dir,seed,c
   print(sprintf("models: %s",toString(models)))
   
   
-  #   #worker.nodes = mpi.universe.size()-1
-  #   #print(sprintf("worker.nodes=%i",worker.nodes))
-  #   dir.path = file.path(paste("/home/amoongat/Projects/Rats-Credit/Sources/logs",ratName, sep = "/"))
-  #   cl <- startMPIcluster(count=count,verbose=TRUE, logdir = dir.path)
-  #   setRngDoMPI(cl, seed=seed) 
-  #   exportDoMPI(cl, c("src.dir","model.data.dir"),envir=environment())
-  #   registerDoMPI(cl)
+    #worker.nodes = mpi.universe.size()-1
+    #print(sprintf("worker.nodes=%i",worker.nodes))
+    dir.path = file.path(paste("/home/amoongat/Projects/Rats-Credit/Sources/logs",ratName, sep = "/"))
+    cl <- startMPIcluster(count=count,verbose=TRUE, logdir = dir.path)
+    setRngDoMPI(cl, seed=seed) 
+    exportDoMPI(cl, c("src.dir","model.data.dir"),envir=environment())
+    registerDoMPI(cl)
     
-  #   cat(sprintf('Running validation with %d worker(s)\n', getDoParWorkers()))
+    cat(sprintf('Running validation with %d worker(s)\n', getDoParWorkers()))
     
-  #   initWorkers <-  function() {
-  #     source(paste(src.dir,"../ModelClasses.R", sep="/"))
-  #     source(paste(src.dir,"PathModel.R", sep="/"))
-  #     source(paste(src.dir,"TurnModel.R", sep="/"))
-  #     source(paste(src.dir,"HybridModel1.R", sep="/"))
-  #     source(paste(src.dir,"HybridModel2.R", sep="/"))
-  #     source(paste(src.dir,"HybridModel3.R", sep="/"))
-  #     source(paste(src.dir,"HybridModel4.R", sep="/"))
-  #     source(paste(src.dir,"../BaseClasses.R", sep="/"))
-  #     source(paste(src.dir,"../exportFunctions.R", sep="/"))
-  #     source(paste(src.dir,"../ModelUpdateFunc.R", sep="/"))
-  #     #attach(myEnv, name="sourced_scripts")
-  #   }
+    initWorkers <-  function() {
+      source(paste(src.dir,"../ModelClasses.R", sep="/"))
+      source(paste(src.dir,"PathModel.R", sep="/"))
+      source(paste(src.dir,"TurnModel.R", sep="/"))
+      source(paste(src.dir,"HybridModel1.R", sep="/"))
+      source(paste(src.dir,"HybridModel2.R", sep="/"))
+      source(paste(src.dir,"HybridModel3.R", sep="/"))
+      source(paste(src.dir,"HybridModel4.R", sep="/"))
+      source(paste(src.dir,"../BaseClasses.R", sep="/"))
+      source(paste(src.dir,"../exportFunctions.R", sep="/"))
+      source(paste(src.dir,"../ModelUpdateFunc.R", sep="/"))
+      #attach(myEnv, name="sourced_scripts")
+    }
     
-  #   opts <- list(initEnvir=initWorkers)
+    opts <- list(initEnvir=initWorkers)
     
   
-  #  generatedDataList <-  
-  #    foreach(i=1:length(models), .options.mpi=opts,.packages = c("rlist","DEoptim","dplyr","TTR"),.export=c("testData")) %:%
-  #    foreach(generation=1:20) %dopar%
-  #    {
-  #      model = models[i] 
-  #      modelName = strsplit(models[i],"\\.")[[1]][1]
-  #      creditAssignment = strsplit(models[i],"\\.")[[1]][2]
-  #      trueModelData = slot(slot(allModelRes,modelName),creditAssignment)
+   generatedDataList <-  
+     foreach(i=1:length(models), .options.mpi=opts,.packages = c("rlist","DEoptim","dplyr","TTR"),.export=c("testData")) %:%
+     foreach(generation=1:20) %dopar%
+     {
+       model = models[i] 
+       modelName = strsplit(models[i],"\\.")[[1]][1]
+       creditAssignment = strsplit(models[i],"\\.")[[1]][2]
+       trueModelData = slot(slot(allModelRes,modelName),creditAssignment)
 
-  #      #trueModelData = modifyModelData(trueModelData) 
-  #      simLearns = FALSE 
-  #      missedOptimalIter = 0
+       #trueModelData = modifyModelData(trueModelData) 
+       simLearns = FALSE 
+       missedOptimalIter = 0
        
-  #      while(!simLearns){
-  #        generated_data = simulateData(trueModelData,ratdata,allModels)
-  #        #end_index = getEndIndex(ratName,generated_data@allpaths, sim=1, limit=0.95)
-  #        simLearns = checkSimLearns(generated_data@allpaths,sim=1,limit=0.8) 
-  #        missedOptimalIter=missedOptimalIter+1
+       while(!simLearns){
+         generated_data = simulateData(trueModelData,ratdata,allModels)
+         #end_index = getEndIndex(ratName,generated_data@allpaths, sim=1, limit=0.95)
+         simLearns = checkSimLearns(generated_data@allpaths,sim=1,limit=0.8) 
+         missedOptimalIter=missedOptimalIter+1
          
-  #        if(missedOptimalIter>500)
-  #        {
-  #           cat(sprintf('model = %s, missedOptimalIter = %i, alpha = %f, gamma = %f\n', model,missedOptimalIter,trueModelData@alpha, trueModelData@gamma1))
-	#     break
-  #        }
-  #        cat(sprintf('model = %s, missedOptimalIter = %i, alpha = %f, gamma = %f\n', model,missedOptimalIter,trueModelData@alpha, trueModelData@gamma1)) 
-  #      }
+         if(missedOptimalIter>500)
+         {
+            cat(sprintf('model = %s, missedOptimalIter = %i, alpha = %f, gamma = %f\n', model,missedOptimalIter,trueModelData@alpha, trueModelData@gamma1))
+	    break
+         }
+         cat(sprintf('model = %s, missedOptimalIter = %i, alpha = %f, gamma = %f\n', model,missedOptimalIter,trueModelData@alpha, trueModelData@gamma1)) 
+       }
        
-  #      if(simLearns)
-  #      {
-  #        generated_data = populateSimRatModel(ratdata,generated_data,modelName)
-  #        generated_data@simModel = trueModelData@Model
-  #        generated_data@simMethod = trueModelData@creditAssignment
-  #        generated_data@simModelData = trueModelData
-  #        generated_data
-  #      }
+       if(simLearns)
+       {
+         generated_data = populateSimRatModel(ratdata,generated_data,modelName)
+         generated_data@simModel = trueModelData@Model
+         generated_data@simMethod = trueModelData@creditAssignment
+         generated_data@simModelData = trueModelData
+         generated_data
+       }
        
-  #    }    
+     }    
  
-  #allData<-unlist(generatedDataList)
-  #modelNum =  length(allData)
+  allData<-unlist(generatedDataList)
+  modelNum =  length(allData)
   
   
-  # time2<- system.time(  
-  #   resList<-
-  #     foreach(i = 1:modelNum, .options.mpi=opts) %:%
-  #     foreach(model = models) %dopar% {
-  #       generated_data = allData[[i]]
-  #       modelName = strsplit(model,"\\.")[[1]][1]
-  #       creditAssignment = strsplit(model,"\\.")[[1]][2]
-  #       modelData =  new("ModelData", Model=modelName, creditAssignment = creditAssignment, sim=1)
-  #       argList<-getArgList(modelData,generated_data)
-  #       np.val = length(argList$lower) * 10
-  #       myList <- DEoptim.control(NP=np.val, F=0.8, CR = 0.9,trace = FALSE, itermax = 200)
-  #       out <-DEoptim(negLogLikFunc,argList$lower,argList$upper,ratdata=argList[[3]],half_index=800,modelData=argList[[5]],testModel = argList[[6]],sim = argList[[7]],myList)
-  #       modelData = setModelParams(modelData, unname(out$optim$bestmem))
-  #       modelData = setModelResults(modelData,generated_data,allModels)
-  #       list(data=generated_data,res=modelData)
-  #     }
-  # )
+  time2<- system.time(  
+    resList<-
+      foreach(i = 1:modelNum, .options.mpi=opts) %:%
+      foreach(model = models) %dopar% {
+        generated_data = allData[[i]]
+        modelName = strsplit(model,"\\.")[[1]][1]
+        creditAssignment = strsplit(model,"\\.")[[1]][2]
+        modelData =  new("ModelData", Model=modelName, creditAssignment = creditAssignment, sim=1)
+        argList<-getArgList(modelData,generated_data)
+        np.val = length(argList$lower) * 10
+        myList <- DEoptim.control(NP=np.val, F=0.8, CR = 0.9,trace = FALSE, itermax = 200)
+        out <-DEoptim(negLogLikFunc,argList$lower,argList$upper,ratdata=argList[[3]],half_index=800,modelData=argList[[5]],testModel = argList[[6]],sim = argList[[7]],myList)
+        modelData = setModelParams(modelData, unname(out$optim$bestmem))
+        modelData = setModelResults(modelData,generated_data,allModels)
+        list(data=generated_data,res=modelData)
+      }
+  )
   
-  # print(time2)
-  # rat = ratdata@rat
-  # save(resList,  file = paste0(res.model.data.dir,"/",rat, format(Sys.time(),'_%Y%m%d_%H%M%S'),"_resList.Rdata"))
+  print(time2)
+  rat = ratdata@rat
+  save(resList,  file = paste0(res.model.data.dir,"/",rat, format(Sys.time(),'_%Y%m%d_%H%M%S'),"_resList.Rdata"))
 
-  setwd(res.model.data.dir)
-  resData=list.files(".",pattern=paste0(ratName, ".*_resList.Rdata"))
-  load(resData)
-  modelNum=length(resList)
-  generatedDataList <- list()
   for(i in 1:modelNum)
   {
     min_method = ""
@@ -148,17 +143,17 @@ HoldoutTestNew=function(ratdata,testData,src.dir,setup.hpc,model.data.dir,seed,c
   save(mat_res, generatedDataList,resList,  file = paste0(res.model.data.dir, "/" , rat, format(Sys.time(),'_%Y%m%d_%H%M%S'),"_mat_res.Rdata"))
   
   
-  # if(setup.hpc)
-  # {
-  #   #stopCluster(cl)
-  #   #stopImplicitCluster()
-  #   closeCluster(cl)
-  # }
-  # else
-  # {
-  #   stopCluster(cl)
-  #   stopImplicitCluster()
-  # }
+  if(setup.hpc)
+  {
+    #stopCluster(cl)
+    #stopImplicitCluster()
+    closeCluster(cl)
+  }
+  else
+  {
+    stopCluster(cl)
+    stopImplicitCluster()
+  }
   
   #boxplotMse(mat_res,model,rat)
   

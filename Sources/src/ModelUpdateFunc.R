a2ba55375ea1936470039c7f59650c233e040364
mattapattu <34416958+mattapattu@users.noreply.github.com>
2022-08-25 16:48:59 +0200

    latest

--- Sources/src/ModelUpdateFunc.R
+++ Sources/src/ModelUpdateFunc.R
@@ -165,7 +165,7 @@ getModelResults=function(ratdata, testingdata, sim, src.dir, model.src, setup.hp
         cat('Create new cluster\n') 
           #cl2 <- getDoMpiCluster()
         np.val = length(argList$lower)*10
-        myList <- DEoptim.control(NP=80, F=0.6, CR = 0.9,trace = FALSE, itermax = 200)
+        myList <- DEoptim.control(NP=np.val, F=0.8, CR = 0.9,trace = FALSE, itermax = 200)
         out <-DEoptim(negLogLikFunc,argList$lower,argList$upper,ratdata=argList[[3]],half_index=800,modelData=argList[[5]],testModel = argList[[6]],sim = argList[[7]],myList)
         cat('model = ',model, ', bestval=',unname(out$optim$bestme),'\n',sep = '')
         cat('model = ',model, ', bestval=',unname(out$optim$bestval),'\n',sep = '')

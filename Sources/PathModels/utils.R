d2829850e5c0470138d5760df753e0af11165845
mattapattu <34416958+mattapattu@users.noreply.github.com>
2022-09-03 23:11:49 +0200

    latest

--- Sources/PathModels/utils.R
+++ Sources/PathModels/utils.R
@@ -1602,19 +1602,19 @@ plotSimParamEstimation=function(ratdata,testData,model.data.dir,plot.dir)
      title <- paste(model, ", nbSim=",nbSims,collapse="")
      plot(xaxis,alpha_upper_bounds,type ='l',lty=2,col="black",ylim=c(0,1),main=title,xlab="Trials",ylab="Parameters",cex.axis = 1.5, cex.lab = 1.3)
      lines(xaxis,alpha_lower_bounds, lty=2, col="black")
-     lines(h=alpha,col="black")
+     abline(h=alpha,col="black")
 
      lines(xaxis,gamma1_upper_bounds, lty=2, col='red')
      lines(xaxis,gamma1_lower_bounds, lty=2, col='red')
-     lines(h=gamma1, col='red')
+     abline(h=gamma1, col='red')
 
      lines(xaxis,gamma2_upper_bounds, lty=2, col='blue')
      lines(xaxis,gamma2_lower_bounds, lty=2, col='blue')
-     lines(h=gamma2, col='blue')
+     abline(h=gamma2, col='blue')
 
      lines(xaxis,lambda_upper_bounds, lty=2, col='green')
      lines(xaxis,lambda_lower_bounds, lty=2, col='green')
-     lines(h=lambda, col='green')
+     abline(h=lambda, col='green')
 
      
      #abline(v=true80,col='green')  

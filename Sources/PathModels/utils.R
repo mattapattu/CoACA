ee22cc4dccc43fc70b3d11d1e1cf2f15db5c26b7
mattapattu <34416958+mattapattu@users.noreply.github.com>
2022-09-03 23:10:55 +0200

    latest

--- Sources/PathModels/utils.R
+++ Sources/PathModels/utils.R
@@ -1508,7 +1508,7 @@ plotSimParamEstimation=function(ratdata,testData,model.data.dir,plot.dir)
      gamma1 = modelRes[[1]][n.rows,3]
      gamma2 = modelRes[[1]][n.rows,4]
      lambda = modelRes[[1]][n.rows,5]
-     rowEnd <- modelRes[[1]][,1]
+     rowEnd <- modelRes[[1]][n.rows,1]
 
     if(model=="Paths")
     {

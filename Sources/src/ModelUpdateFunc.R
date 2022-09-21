c012ccef58e421e2f48ebb9ed9b247da40ba37fb
mattapattu <34416958+mattapattu@users.noreply.github.com>
2022-09-21 15:08:30 +0200

    latest

--- Sources/src/ModelUpdateFunc.R
+++ Sources/src/ModelUpdateFunc.R
@@ -58,7 +58,7 @@ analyzeParamSpace=function(ratdata,testData,src.dir,model.src,setup.hpc,model.da
     opts <- list(initEnvir=initWorkers) 
   
   alpha_seq = seq_log(1e-3, 1e-2,10)
-  gamma1_seq = seq_log(1e-8, 1e-4, 100)
+  gamma1_seq = seq_log(1e-8, 1e-4, 200)
   iter=c(300,800,1100,1500,length(ratdata@allpaths[,1]))
   
   resMat <- foreach(i = 1:length(models), .combine='rbind', .inorder=TRUE, .options.mpi=opts) %:%
@@ -108,9 +108,65 @@ analyzeParamSpace=function(ratdata,testData,src.dir,model.src,setup.hpc,model.da
     }
   }
   
-  print(resMat)
-  rat = ratdata@rat
-  save(resMat, file = paste0(model.data.dir,"/",rat, format(Sys.time(),'_%Y%m%d_%H%M%S'),"_resMat.Rdata")) 
+
+  #iter = c(300,800,1100,1500,length(ratdata@allpaths[,1]))
+  df <- as.data.frame(resMat)
+  cols.num <- c(1,3,4,5,6,7)
+  df[,cols.num] <- lapply(cols.num,function(x) as.numeric(df[[x]]))
+  models = c("Paths","Hybrid1","Hybrid2","Hybrid3","Hybrid4","Turns")
+
+  minDfModels <- foreach(model = models,.combine='rbind', .inorder=TRUE) %:% 
+    foreach(it = iter,.combine='rbind', .inorder=TRUE) %do%
+  {
+    df_it <- df[which(df[,1]==it & df[,2]==model),]
+    min_lik1 = 1000000
+    minmodel = modelData <- new("ModelData", Model = model, creditAssignment = "qlearningAvgRwd", sim = 2)
+    for(idx in 1:length(df_it[,1]))
+    {
+      modelData@alpha = df_it[idx,3]
+      modelData@gamma1 = df_it[idx,4]
+      modelData@gamma2 = 0.1
+      modelData@lambda = 0
+      argList <- getArgList(modelData, ratdata)
+      lik <- TurnsNew::getTurnsLikelihood(ratdata, modelData, argList[[6]], sim=2)
+      lik1 <- sum(lik[c(1:800)])*-1
+      lik2 <- sum(lik[-c(1:800)])*-1
+      df_it[idx,7]= lik1
+      
+      if (is.infinite(lik1)) {
+        lik1= 1000000
+      }else if (is.nan(lik1)) {
+        #print(sprintf("Alpha = %f", alpha))
+        lik1 = 1000000
+      }else if (is.na(lik1)) {
+        #print(sprintf("Alpha = %f, Gamma1=%f", alpha,gamma1))
+        lik1 = 1000000
+      }
+      if(lik1 < min_lik1)
+      {
+        min_lik1=lik1
+        min_lik2=lik2
+        minmodel@alpha = df_it[idx,3]
+        minmodel@gamma1 = df_it[idx,4]
+        minmodel@gamma2 = 0.1
+        minmodel@lambda = 0
+      }
+        
+      
+      
+      
+     }
+    c(model,it,minmodel@alpha,minmodel@gamma1,minmodel@gamma2,minmodel@lambda,min_lik1,min_lik2)
+      
+  }
+  
+  print(minDfModels)
+  save(minDfModels, file = paste0(model.data.dir,"/",ratdata@rat, format(Sys.time(),'_%Y%m%d_%H%M%S'),"_minDfModels.Rdata")) 
+  
+
+  # print(resMat)
+  # rat = ratdata@rat
+  # save(resMat, file = paste0(model.data.dir,"/",rat, format(Sys.time(),'_%Y%m%d_%H%M%S'),"_resMat.Rdata")) 
 
   
 }

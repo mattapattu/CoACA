53a4bf2c29f08bb462645aa79479288d9558ce3c
mattapattu <34416958+mattapattu@users.noreply.github.com>
2022-09-23 10:39:54 +0200

    latest

--- Sources/src/ModelUpdateFunc.R
+++ Sources/src/ModelUpdateFunc.R
@@ -58,8 +58,8 @@ analyzeParamSpace=function(ratdata,testData,src.dir,model.src,setup.hpc,model.da
   opts <- list(initEnvir=initWorkers) 
   
   
-  alpha_seq = seq_log(1e-3, 1e-2,10)
-  gamma1_seq = seq_log(1e-8, 1e-4, 200)
+  alpha_seq = seq_log(1e-3, 1e-2,20)
+  gamma1_seq = seq_log(1e-8, 1e-4, 100)
   iter=c(seq(from = 0, to = length(ratdata@allpaths[,1]), by = 400)[-1],length(ratdata@allpaths[,1]))
   
   gridMat<- expand.grid(alpha_seq,gamma1_seq,iter,models,stringsAsFactors = FALSE)

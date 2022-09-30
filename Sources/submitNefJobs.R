606ed17b5d299e4a6e5cadc6b44367932bcf55dc
mattapattu <34416958+mattapattu@users.noreply.github.com>
2022-09-30 10:05:53 +0200

    latest

--- Sources/submitNefJobs.R
+++ Sources/submitNefJobs.R
@@ -8,67 +8,19 @@ rat <- as.integer(args[1])
 options(error=function()traceback(2))
 
 
-
-################ INIT ####################################################################
-rats = c("rat_101","rat_103","rat_106","rat_112","rat_113","rat_114","robert")
-names=c('e','f','g','c','d','h','i','j','a','b','k')
-
-### Options Linux/Windows ####
-
-src.dir = file.path("/home/amoongat/Projects/Rats-Credit/Sources/src")
-
-model = "Model2"  ## {Model1,Model2,Model3}
-source(paste(src.dir,"ModelClasses.R", sep="/"))
-
-## Model files
-model.src = paste(src.dir,model, sep="/")
-source(paste(model.src,"PathModel.R", sep="/"))
-source(paste(model.src,"TurnModel.R", sep="/"))
-source(paste(model.src,"HybridModel1.R", sep="/"))
-source(paste(model.src,"HybridModel2.R", sep="/"))
-source(paste(model.src,"HybridModel2.R", sep="/"))
-source(paste(model.src,"HybridModel3.R", sep="/"))
-source(paste(model.src,"HybridModel4.R", sep="/"))
-source(paste(src.dir,"BaseClasses.R", sep="/"))
-source(paste(src.dir,"ModelUpdateFunc.R", sep="/"))
-source(paste(src.dir,"ValidationFunc.R", sep="/"))
-source(paste(src.dir,"ValidationFunc2.R", sep="/"))
-source(paste(src.dir,"../PathModels/utils.R", sep="/"))
-
-
-
-data.path = file.path("/home/amoongat/Projects/Rats-Credit/Data/new_data_journeys.Rdata")
-load(data.path)
-plot.dir = file.path("/home/amoongat/Projects/Rats-Credit/Plots")
-model.data.dir = file.path("/home/amoongat/Projects/Rats-Credit/Data/Rat_Model_Data")
-
-rawData <- donnees_ash[[rat]]
-enregres = enregCombine(rawData,rats[rat])
-allpaths = enregres$allpaths
-boxTimes = enregres$boxTimes
-ratdata = populateRatModel(allpaths=allpaths,rat=rats[rat],donnees_ash[[rat]],TurnModel)
-  
-############## END INIT ##############################################  
-
-testData = new("TestModels", Name = "AvgRwd",Models=c("Paths.qlearningAvgRwd","Hybrid1.qlearningAvgRwd","Hybrid2.qlearningAvgRwd","Hybrid3.qlearningAvgRwd","Hybrid4.qlearningAvgRwd","Turns.qlearningAvgRwd"))
-
 #### Tests ##############
 computeModelParams = F
 generateModelParamMat = F
 paramEstTest = T
+validateHoldout = T
 
 ########################## Test 1: computeModelParams 
 
 if(computeModelParams){
-  alpha_seq = seq_log(1e-3, 0.9,60)
-  gamma1_seq = seq_log(1e-8, 1e-4, 10)
-  iters=c(seq(from = 0, to = length(allpaths[,1]), by = 400)[-1],length(allpaths[,1]))
-  models = testData@Models
-  gridMat<- expand.grid(alpha_seq,gamma1_seq,iters,models,stringsAsFactors = FALSE)
 
-    # each slave node ~ 400 iterations
+  currentTest = "computeModelParams"
+  source("testConfig.R")
 
-  sequences = seq(0,length(gridMat[,1]), length.out=10)
   cores = 10
   walltime = "7:00"
 
@@ -82,7 +34,7 @@ if(computeModelParams){
       name = paste0("mParams",i,"_",paste0("rat",rat))
       stdout = paste0("\'logs/",name,"_%jobid%.stdout\'")
       stderr = paste0("\'logs/",name,"_%jobid%.stderr\'")
-      currentTest = "computeModelParams"
+      
 
       command <- sprintf("oarsub -t besteffort -t idempotent -l /nodes=1/core=%i,walltime=%s -n %s --stdout=%s --stderr=%s -S \"./ratscript2.sh %i %i %i %s %i %i\" ", cores, walltime,name,stdout,stderr,rat,seed,spawnslaves,currentTest, start_idx, end_idx)
       cat(command)
@@ -95,6 +47,10 @@ if(computeModelParams){
 
 
 if(generateModelParamMat){
+
+  currentTest = "generateModelParamMat"
+  source("testConfig.R")
+
   cores = 5
   walltime = "1:00"
   spawnslaves = cores-1
@@ -120,27 +76,11 @@ if(paramEstTest)
     #allmodelRes = readModelParams(ratdata,model.data.dir,testData, sim=2)
     #testParamEstimation(ratdata,allmodelRes,testData,model.src,setup.hpc,model.data.dir,seed,count)
     
-   n = 8
-   sessions<-unique(ratdata@allpaths[,5])
-   session_grps<-split(sessions, sort(sessions%%8))
-   maxVecs <- c()
-   for(grp in c(1:n))
-   {
-    print(grp)
-    begin_ses <- min(session_grps[[grp]])
-    end_ses <- max(session_grps[[grp]])
-    indices_of_ses <- which(ratdata@allpaths[,5]>=begin_ses & ratdata@allpaths[,5] <=end_ses)
-    maxVecs <- c(maxVecs,max(indices_of_ses))
-   }
+  currentTest = "paramEstTest"
+  source("testConfig.R")
+  cores = 10
+  walltime = "10:00"
 
-
-    alpha_seq = seq_log(1e-3, 0.9,60)
-    gamma1_seq = seq_log(1e-8, 1e-4, 10)
-    gridMat<- expand.grid(alpha_seq,gamma1_seq,maxVecs,c(1:40),stringsAsFactors = FALSE)
-    
-    sequences = seq(0,length(gridMat[,1]), length.out=21)
-    cores = 10
-    walltime = "10:00"
 
    for(i in c(1:20))
    {
@@ -152,16 +92,12 @@ if(paramEstTest)
       name = paste0("pEstTest",i,"_",paste0("rat",rat))
       stdout = paste0("\'logs/",name,"_%jobid%.stdout\'")
       stderr = paste0("\'logs/",name,"_%jobid%.stderr\'")
-      currentTest = "paramEstTest"
 
       command <- sprintf("oarsub -t besteffort -t idempotent -l /nodes=1/core=%i,walltime=%s -n %s --stdout=%s --stderr=%s -S \"./ratscript2.sh %i %i %i %s %i %i\" ", cores, walltime,name,stdout,stderr,rat,seed,spawnslaves,currentTest, start_idx, end_idx)
       cat(command)
       cat("\n")
       system(command)
    }
-
-    
-    
 
 }  
 
@@ -170,17 +106,9 @@ if(paramEstTest)
 
 if(validateHoldout)
   {
-    #debug(HoldoutTest)
-    #allmodelRes = readModelParams(ratdata,model.data.dir,testData, sim=2)
-    #HoldoutTest(ratdata,allmodelRes,testData,model.src,setup.hpc,model.data.dir,seed,count)
-    
-    
-    alpha_seq = seq_log(1e-3, 0.9,60)
-    gamma1_seq = seq_log(1e-8, 1e-4, 10)
-    models = testData@Models
-    gridMat<- expand.grid(alpha_seq,gamma1_seq,models,c(1:100),stringsAsFactors = FALSE)
-    
-    sequences = seq(0,length(gridMat[,1]), length.out=21)
+   
+    currentTest = "validateHoldout"
+    source("testConfig.R")  
     cores = 10
     walltime = "10:00"
 
@@ -194,7 +122,6 @@ if(validateHoldout)
       name = paste0("hValid",i,"_",paste0("rat",rat))
       stdout = paste0("\'logs/",name,"_%jobid%.stdout\'")
       stderr = paste0("\'logs/",name,"_%jobid%.stderr\'")
-      currentTest = "paramEstTest"
 
       command <- sprintf("oarsub -t besteffort -t idempotent -l /nodes=1/core=%i,walltime=%s -n %s --stdout=%s --stderr=%s -S \"./ratscript2.sh %i %i %i %s %i %i\" ", cores, walltime,name,stdout,stderr,rat,seed,spawnslaves,currentTest, start_idx, end_idx)
       cat(command)
---
+++ Sources/testConfig.R
@@ -0,0 +1,91 @@
+rats = c("rat_101","rat_103","rat_106","rat_112","rat_113","rat_114","robert")
+names=c('e','f','g','c','d','h','i','j','a','b','k')
+
+src.dir = file.path("/home/amoongat/Projects/Rats-Credit/Sources/src")
+
+data.path = file.path("/home/amoongat/Projects/Rats-Credit/Data/new_data_journeys.Rdata")
+load(data.path)
+
+plot.dir = file.path("/home/amoongat/Projects/Rats-Credit/Plots")
+model.data.dir = file.path("/home/amoongat/Projects/Rats-Credit/Data/Rat_Model_Data")
+#model.data.dir = file.path("/home/amoongat/Projects/Rats-Credit/Data/Rat_Model_Data/qlearning")
+
+model = "Model2"  ## {Model1,Model2,Model3}
+source(paste(src.dir,"ModelClasses.R", sep="/"))
+
+setup.hpc = TRUE
+
+## Model files
+model.src = paste(src.dir,model, sep="/")
+source(paste(model.src,"PathModel.R", sep="/"))
+source(paste(model.src,"TurnModel.R", sep="/"))
+source(paste(model.src,"HybridModel1.R", sep="/"))
+source(paste(model.src,"HybridModel2.R", sep="/"))
+source(paste(model.src,"HybridModel2.R", sep="/"))
+source(paste(model.src,"HybridModel3.R", sep="/"))
+source(paste(model.src,"HybridModel4.R", sep="/"))
+
+
+source(paste(src.dir,"BaseClasses.R", sep="/"))
+source(paste(src.dir,"ModelUpdateFunc.R", sep="/"))
+source(paste(src.dir,"ValidationFunc.R", sep="/"))
+source(paste(src.dir,"ValidationFunc2.R", sep="/"))
+source(paste(src.dir,"../PathModels/utils.R", sep="/"))
+
+rawData <- donnees_ash[[select_rat]] 
+enregres = enregCombine(rawData,rats[select_rat])
+allpaths = enregres$allpaths
+boxTimes = enregres$boxTimes
+    
+ratdata = populateRatModel(allpaths=allpaths,rat=rats[select_rat],donnees_ash[[select_rat]],TurnModel)
+
+############### Tests #############################################
+
+
+
+if(currentTest == "computeModelParams")
+{
+  alpha_seq = seq_log(1e-3, 0.9,60)
+  gamma1_seq = seq_log(1e-8, 1e-4, 10)
+  iters=c(seq(from = 0, to = length(allpaths[,1]), by = 400)[-1],length(allpaths[,1]))
+  models = testData@Models
+  gridMat<- expand.grid(alpha_seq,gamma1_seq,iters,models,stringsAsFactors = FALSE)
+  sequences = seq(0,length(gridMat[,1]), length.out=11)    
+}
+
+################# Test : Param estimation test ######################
+
+
+if(currentTest == "paramEstTest")
+{
+  n = 8
+  sessions<-unique(ratdata@allpaths[,5])
+  session_grps<-split(sessions, sort(sessions%%8))
+  maxVecs <- c()
+  for(grp in c(1:n))
+  {
+   print(grp)
+   begin_ses <- min(session_grps[[grp]])
+   end_ses <- max(session_grps[[grp]])
+   indices_of_ses <- which(ratdata@allpaths[,5]>=begin_ses & ratdata@allpaths[,5] <=end_ses)
+   maxVecs <- c(maxVecs,max(indices_of_ses))
+  }
+
+  alpha_seq = seq_log(1e-3, 0.9,60)
+  gamma1_seq = seq_log(1e-8, 1e-4, 10)
+  gridMat<- expand.grid(alpha_seq,gamma1_seq,maxVecs,c(1:40),stringsAsFactors = FALSE)
+  sequences = seq(0,length(gridMat[,1]), length.out=21)
+}
+
+
+################# Test : validate holdout ######################
+
+if(currentTest == "paramEstTest")
+{
+  alpha_seq = seq_log(1e-3, 0.9,60)
+  gamma1_seq = seq_log(1e-8, 1e-4, 10)
+  models = testData@Models
+  gridMat<- expand.grid(alpha_seq,gamma1_seq,models,c(1:100),stringsAsFactors = FALSE)
+  sequences = seq(0,length(gridMat[,1]), length.out=21)
+
+}

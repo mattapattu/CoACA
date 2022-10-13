5670fb0619be3f8c7e5cea2e462cad6d83bd1310
mattapattu <34416958+mattapattu@users.noreply.github.com>
2022-10-13 15:22:06 +0200

    latest

--- Sources/src/ValidationFunc3.R
+++ Sources/src/ValidationFunc3.R
@@ -501,9 +501,9 @@ combineParamEstResLists=function(ratdata,testData,src.dir,model.src,setup.hpc,mo
 
     
   iters=c(seq(from = 0, to = length(ratdata@allpaths[,1]), by = 400)[-1],length(ratdata@allpaths[,1]))  
-  chunkSize = ceiling(length(models)*length(iters)/getDoParWorkers())
+  #chunkSize = ceiling(length(models)*length(iters)/getDoParWorkers())
   print(sprintf("chunkSize=%i",chunkSize))
-
+  chunkSize=10
   opts <- list(initEnvir=initWorkers,chunkSize=chunkSize) 
 
   df <- as.data.frame(resMat)

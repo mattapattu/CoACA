bc81f87695ce4aef696919871a1e3be62d6d1498
mattapattu <34416958+mattapattu@users.noreply.github.com>
2022-10-22 12:33:25 +0200

    latest

--- Sources/src/ValidationFunc3.R
+++ Sources/src/ValidationFunc3.R
@@ -524,7 +524,7 @@ combineHoldoutResLists=function(ratdata,testData,src.dir,model.src,setup.hpc,mod
         df_genData_model = df_genData[which(df_genData[,1]==model),]
         modelName = strsplit(model,"\\.")[[1]][1]
         creditAssignment = strsplit(model,"\\.")[[1]][2]
-        cat(sprintf('rat=%s, genDataFile=%i, genDataNum = %i, trueModel = %s, modelName = %s\n', ratName,genDataFile,genDataNum, generatedData@simModel,modelName))
+        #cat(sprintf('rat=%s, genDataFile=%i, genDataNum = %i, trueModel = %s, modelName = %s\n', ratName,genDataFile,genDataNum, generatedData@simModel,modelName))
 
         for(idx in 1:length(df_genData_model[,1]))
         {

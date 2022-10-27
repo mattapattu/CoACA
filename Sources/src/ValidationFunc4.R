6b5a24e8baf00665c1d2f681a0d1ca0166c21991
mattapattu <34416958+mattapattu@users.noreply.github.com>
2022-10-27 22:37:43 +0200

    latest

--- Sources/src/ValidationFunc4.R
+++ Sources/src/ValidationFunc4.R
@@ -159,8 +159,7 @@ initWorkers <-  function() {
    #chunkSize = 150
    opts <- list(initEnvir=initWorkers,chunkSize=chunkSize) 
  
-   source(paste(src.dir,"../exportFunctions.R", sep="/")) 
-   
+  
    generatedDataList <-  
    foreach(i=1:length(gridMat[,1]), .options.mpi=opts,.packages = c("rlist","DEoptim","dplyr","TTR"),.export=c("testData")) %dopar%
    {

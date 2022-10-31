212181bf3a434df4c932be6addf95ad666458d7a
mattapattu <34416958+mattapattu@users.noreply.github.com>
2022-10-31 11:24:58 +0100

    latest

--- Sources/src/ValidationFunc4.R
+++ Sources/src/ValidationFunc4.R
@@ -587,6 +587,21 @@ testParamEstimationV4=function(ratdata,testData,src.dir,model.src,setup.hpc,mode
    
   dir.path = file.path(paste("/home/amoongat/Projects/Rats-Credit/Sources/logs",ratName, sep = "/"))
   timestamp = format(Sys.time(),'_%Y%m%d_%H%M%S')
+
+  setwd(gen.data.dir)
+  dfData <- list.files(".", pattern=paste0(ratName,".*genDataset.Rdata"), full.names=FALSE)
+  #dfData <- dfData[which(str_detect(dfData,paste0("GenData",genDataList,"_")))]
+  genDataFiles <- list()
+  for(i in 1:length(dfData))
+  {
+    pattern=paste0(ratName,"_GenData",i,"_.*Rdata")
+    #print(pattern)
+    res=list.files(".", pattern=pattern, full.names=FALSE)
+    load(res)
+    genDataFiles[[i]] <- allData
+
+    #genDataFiles[[i]] <- get(load(dfData[[i]]))
+  }
  
   cl <- startMPIcluster(count=count,verbose=TRUE, logdir = dir.path)
   setRngDoMPI(cl, seed=seed)
@@ -612,22 +627,6 @@ testParamEstimationV4=function(ratdata,testData,src.dir,model.src,setup.hpc,mode
     source(paste(src.dir,"exportFunctions.R", sep="/"))
 
   
-
-  setwd(gen.data.dir)
-  dfData <- list.files(".", pattern=paste0(ratName,".*genDataset.Rdata"), full.names=FALSE)
-  #dfData <- dfData[which(str_detect(dfData,paste0("GenData",genDataList,"_")))]
-  genDataFiles <- list()
-  for(i in 1:length(dfData))
-  {
-    pattern=paste0(ratName,"_GenData",i,"_.*Rdata")
-    #print(pattern)
-    res=list.files(".", pattern=pattern, full.names=FALSE)
-    load(res)
-    genDataFiles[[i]] <- allData
-
-    #genDataFiles[[i]] <- get(load(dfData[[i]]))
-  }
-
   chunkSize = ceiling(length(gridMat[,1])/(getDoParWorkers()))
   #chunkSize = 300
   opts <- list(initEnvir=initWorkers,chunkSize=chunkSize, profile=FALSE) 

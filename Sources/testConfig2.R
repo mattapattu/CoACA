f135a7f12d41f73cba1f2e928d249cbe4b99a420
mattapattu <34416958+mattapattu@users.noreply.github.com>
2022-11-09 16:19:12 +0100

    latest

--- Sources/testConfig2.R
+++ Sources/testConfig2.R
@@ -131,6 +131,7 @@ source(paste(src.dir,"ModelUpdateFunc2.R", sep="/"))
 #source(paste(src.dir,"ValidationFunc.R", sep="/"))
 #source(paste(src.dir,"ValidationFunc2.R", sep="/"))
 source(paste(src.dir,"ValidationFunc4.R", sep="/"))
+source(paste(src.dir,"ValidationFunc5.R", sep="/"))
 source(paste(src.dir,"../PathModels/utils.R", sep="/"))
 
 testData = new("TestModels", Name = testSuite,Models=testModels)

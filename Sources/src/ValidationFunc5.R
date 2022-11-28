77ab8046f0adab76e120d34c496d5d3fcb706723
mattapattu <34416958+mattapattu@users.noreply.github.com>
2022-11-28 12:02:33 +0100

    latest

--- Sources/src/ValidationFunc5.R
+++ Sources/src/ValidationFunc5.R
@@ -227,7 +227,7 @@ combinemultiHoldoutValidation=function(ratdata,data.dir,model.data.dir,count, ge
   holdoutResLists1 <- list.files(".", pattern=paste0(ratName,".*HoldoutResList.Rdata"), full.names=FALSE)
   for(i in c(1:length(holdoutResLists1)))
   {
-    pattern=paste0(ratName,".*","_.*_HoldoutResList.Rdata")
+    pattern=paste0(ratName,".*",i,"_.*_HoldoutResList.Rdata")
     resList=list.files(".", pattern=pattern, full.names=FALSE)
     print(resList)
     #print(any(!complete.cases(resList)))
